dc8113bdd2cd8671a7aafe736f1a8e950b60e596
Voskan Voskanyan <voskanvoskanyan@yandex.ru>
2020-12-25 19:12:35 +0400

    Add database

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # BigCity
+
+Find adventures nearby or in faraway places and access unique homes, experiences, and places around the world.
+
+## Server
+<ul>
+  <li>Node.js</li>
+  <li>Express</li>
+  <li>GraphQL (Apollo)</li>
+  <li>TypeScript</li>
+  <li>MongoDB</li>
+</ul>
+
+## Client
+<ul>
+  <li>React</li>
+  <li>Redux</li>
+  <li>GraphQL</li>
+  <li>TypeScript</li>
+  <li>Ant Design</li>
+</ul>
--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,1 +1,18 @@
 import { MongoClient } from "mongodb";
+
+const user = 'user_name';
+const password = 'password';
+const cluster = 'cluster';
+const dbName = 'main';
+
+const url = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+
+  const db = client.db("main");
+
+  return {
+    listings: db.collection("test_listings")
+  }
+};
