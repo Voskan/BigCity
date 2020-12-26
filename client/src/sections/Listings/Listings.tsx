c64abbe099bd1bce936469c571310da30c1b10e6
Voskan Voskanyan <voskanvoskanyan@yandex.ru>
2020-12-26 18:02:52 +0400

    Client: Add listing dleting functionality

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { server } from '../../lib/api';
-import { ListingsData } from './types';
+import { DeleteListingData, DeleteListingVariables, ListingsData } from './types';
 
 const LISTINGS = `
   query Listings {
@@ -18,6 +18,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
@@ -28,10 +36,19 @@ export const Listings = ({ title }: Props) => {
     console.log(data);
   };
 
+  const deleteListings = async () => {
+    const { data } = await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: { id: "5fe6f7dfc837bc1927ed1589" }
+    });
+    console.log(data);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
-      <button onClick={fetchListings}>Click hire</button>
+      <button onClick={fetchListings}>Get listings</button>
+      <button onClick={deleteListings}>Delete listing</button>
     </div>
   )
 };
