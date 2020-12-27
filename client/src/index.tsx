19bf1c8420d259e77ee9a404d8cb212e9fe35028
Voskan Voskanyan <voskanvoskanyan@yandex.ru>
2020-12-27 19:36:16 +0400

    Client: Add apollo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import reportWebVitals from './reportWebVitals';
 import { Listings } from './sections';
 
+const client = new ApolloClient({
+  uri: "/api"
+});
+
 render(
   <React.StrictMode>
-    <Listings title="BigCity Listings" />
+    <ApolloProvider client={client}>
+      <Listings title="BigCity Listings" />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
